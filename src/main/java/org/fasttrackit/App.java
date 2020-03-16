1db5ad64a50cf930e6d2e5c6ffd60c8f7b5ec823
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-03-16 21:07:25 +0200

    studied encapsulation

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -35,27 +35,29 @@ public class App {
 //
 //        System.out.println("Welcome to the racing game!");
 //
-//        Engine engine1 = new Engine();
-//        engine1.manufacturer = "VW";
-//        engine1.capacity = 2000;
-//
-//        Car carReference = new Car(engine1);
-//        carReference.name = "Audi";
-//        carReference.color = "red";
-//        carReference.maxSpeed = 260;
-//        carReference.mileage = 6.2;
-//        carReference.fuelLevel = 100;
-//        carReference.damaged = true;
-//
-////        carReference.engine =  new Engine();
-////        carReference.engine.manufacturer = "VW";
-////        carReference.engine.capacity = 2000;
-//
-//        double currentDistance = carReference.accelerate(60, 1);
-//        System.out.println("Current distance: " + currentDistance);
-//
-//        Mechanic mechanic = new Mechanic();
-//        mechanic.repair(carReference);
+        Engine engine1 = new Engine();
+        engine1.manufacturer = "VW";
+        engine1.capacity = 2000;
+
+        Car carReference = new Car(engine1);
+        carReference.setName("Audi");
+        carReference.setColor("red");
+        carReference.setMaxSpeed(260);
+        carReference.setMileage(6.2);
+        carReference.setFuelLevel(100);
+        carReference.setDamaged(true);
+
+        System.out.println("Name: " + carReference.getName());
+
+//        carReference.engine =  new Engine();
+//        carReference.engine.manufacturer = "VW";
+//        carReference.engine.capacity = 2000;
+
+        double currentDistance = carReference.accelerate(60, 1);
+        System.out.println("Current distance: " + currentDistance);
+
+        Mechanic mechanic = new Mechanic();
+        mechanic.repair(carReference);
 //
 //        System.out.println("Is car damaged? " + carReference.damaged);
 //
