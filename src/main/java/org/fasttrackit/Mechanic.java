3bd32700b10cdfd7fb7e6376cbaf76997509e0d0
Flaviu Ratiu <ratiu.flaviu@gmail.com>
2020-03-23 18:40:42 +0200

    fixed compilation errors caused by encapsulation

--- src/main/java/org/fasttrackit/Mechanic.java
+++ src/main/java/org/fasttrackit/Mechanic.java
@@ -3,9 +3,9 @@ package org.fasttrackit;
 public class Mechanic {
 
     public void repair(Vehicle vehicle) {
-        System.out.println("Repairing vehicle " + vehicle.name);
+        System.out.println("Repairing vehicle " + vehicle.getName());
 
-        vehicle.totalDistance--;
-        vehicle.damaged = false;
+        vehicle.setTotalDistance(vehicle.getTotalDistance() - 1);
+        vehicle.setDamaged(false);
     }
 }
